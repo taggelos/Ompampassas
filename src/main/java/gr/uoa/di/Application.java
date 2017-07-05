b804012afcc6bd7b0a64354f02fa9603c9dc67eb
Emilios Kalantzis <emkal21@gmail.com>
2017-07-06 01:01:40 +0300

    changes in gradle

--- src/main/java/gr/uoa/di/Application.java
+++ src/main/java/gr/uoa/di/Application.java
@@ -2,12 +2,19 @@ package gr.uoa.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import java.io.File;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static final String imagedir = System.getProperty("user.dir") + "/src/main/webapp/assets/imagedir/";
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         new File(imagedir).mkdirs();
